4d4438bcc085289431afd5683e28c37da1c1b55b
Leo79Jp <leojparodi@gmail.com>
2024-07-29 22:35:25 -0300

    .

---
+++ ejercicio01.js
@@ -0,0 +1,12 @@
+// Suma de dos números:
+
+// Escriba un programa en JavaScript con una función que tome como parámetros dos enteros, los sume y devuelva como resultado la suma. El programa deberá pedir al usuario 
+//los dos números, luego llamar a la función con los números leídos como argumentos y, por último, informar al usuario el valor de la suma.
+
+const prompt = require('prompt-sync')({ siging: true })
+
+a = parseInt(prompt(`Ingrese el primer numero: `))
+b = parseInt(prompt(`Ingrese el segundo numero" : `))
+
+const sumar = ( a, b ) => a + b
+console.log(`La suma es: ${sumar(a, b)}`);
---
+++ ejercicio02.js
@@ -0,0 +1,18 @@
+// Suma con tercer argumento:
+
+// Escriba un programa en JavaScript similar al anterior, pero esta vez utilizando una función que tome un tercer argumento y coloque el resultado de la suma en dicho parámetro.
+const prompt = require('prompt-sync')({ siging: true })
+
+a = parseInt(prompt(`Ingrese el primer numero: `))
+b = parseInt(prompt(`Ingrese el segundo numero : `))
+
+const operacion = ( a, b, resultado) => {
+    return a, b, resultado
+
+const suma = ( a, b, resultado) => resultado = a + b
+
+ }
+ console.log(`La suma es: ${operacion(a, b, suma)}`);
+
+
+ 
